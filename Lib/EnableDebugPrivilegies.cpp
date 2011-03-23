43e1adb208c855712ae696c2e72fbee7cab33f9b
nCdy <X@nCdy.org>
2011-03-23 23:32:48 +0300

    bit smarter EDP

--- Lib/EnableDebugPrivilegies.cpp
+++ Lib/EnableDebugPrivilegies.cpp
@@ -1,10 +1,21 @@
-void EnableDebugPriv( )
-{HANDLE hToken;
-LUID sedebugnameValue;
-TOKEN_PRIVILEGES tkp;
-OpenProcessToken( GetCurrentProcess( ), TOKEN_ADJUST_PRIVILEGES |TOKEN_QUERY, &hToken );
-LookupPrivilegeValue( NULL, SE_DEBUG_NAME, &sedebugnameValue );
-tkp.PrivilegeCount = 1;tkp.Privileges[0].Luid = sedebugnameValue;
-tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-AdjustTokenPrivileges( hToken, false, &tkp, sizeof( tkp ), NULL, NULL );
-CloseHandle( hToken );}
+void EnableDebugPrivilegies( )
+{
+	HANDLE hToken;
+	LUID sedebugnameValue;
+	TOKEN_PRIVILEGES tkp;
+
+	if ( ! OpenProcessToken( GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken ) )
+		return;
+	if ( ! LookupPrivilegeValue( NULL, SE_DEBUG_NAME, &sedebugnameValue ) )
+	{
+		CloseHandle( hToken );
+		return;
+	}
+
+	tkp.PrivilegeCount = 1;
+	tkp.Privileges[0].Luid = sedebugnameValue;
+	tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+	if ( ! AdjustTokenPrivileges( hToken, FALSE, &tkp, sizeof tkp, NULL, NULL ) )
+		CloseHandle( hToken );
+}
