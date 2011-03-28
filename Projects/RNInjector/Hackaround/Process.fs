003b468cb54fdee51934057fa4ed8f6fd6cc5ead
nCdy <X@nCdy.org>
2011-03-28 11:12:30 +0300

    RNInjector getModule

--- Projects/RNInjector/Hackaround/Process.fs
+++ Projects/RNInjector/Hackaround/Process.fs
@@ -7,3 +7,19 @@ open System.Runtime.InteropServices
 let getProcess pc =   
     Process.GetProcesses() |> fun pcs ->
         pcs |> Array.find(fun p -> p.ProcessName = pc)
+
+let getModule m pc =
+    getProcess(pc) |> fun pcs -> 
+        if pcs <> null then
+            [for mx in pcs.Modules ->
+                if mx.ModuleName.ToLower() = m then
+                    Some(mx)
+                else
+                    None ]
+            |> Seq.choose id 
+            |> Seq.head
+            |> fun mdls ->
+                if mdls <> null then
+                    mdls.BaseAddress.ToInt32()
+                else 0
+        else 0
