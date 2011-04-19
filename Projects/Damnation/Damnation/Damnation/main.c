c1380f681aa105a6833b16873f47c0534f75ad66
nCdy <X@nCdy.org>
2011-04-19 15:33:00 +0300

    True WIN API Example

---
+++ Projects/Damnation/Damnation/Damnation/main.c
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "header.h"
+
+BOOL GimmeNative(){
+HMODULE hObsolete  = GetModuleHandle("ntdll.dll");
+*(FARPROC *)&NtAwesomeFunction = GetProcAddress(hObsolete, "NtAwesomeFunction");
+return 0;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    MY_AWESOME_STRUCTURE argument1;
+    PCWSTR argument2;
+    GimmeNative();
+    NtAwesomeFunction(argument1, argument2);
+    return 0;
+}
