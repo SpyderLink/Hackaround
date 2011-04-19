2bd969f3edce798c0d035e17cc874fa82aa4669d
nCdy <X@nCdy.org>
2011-04-19 14:52:58 +0300

    NTAPI

---
+++ Projects/Damnation/Damnation/Damnation/header.h
@@ -0,0 +1,17 @@
+#include "stdafx.h";
+
+#ifndef NTAPI
+    typedef  WINAPI NTAPI;
+#endif
+
+typedef struct _MY_AWESOME_STRUCTURE {
+    USHORT Argument;
+    USHORT NextArgument;
+    PWSTR  Whatever;
+} MY_AWESOME_STRUCTURE;
+
+typedef MY_AWESOME_STRUCTURE *PMY_AWESOME_STRUCTURE;
+
+NTSTATUS
+(NTAPI *NtAwesomeFunction)
+( PMY_AWESOME_STRUCTURE  Something ,PCWSTR NextSomething);
